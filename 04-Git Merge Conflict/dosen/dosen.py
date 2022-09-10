5bcd429aa9a59db38ab38878573bdf2c5ebb3103
daniels66 <danielsaputra814@gmail.com>
2022-09-10 21:03:55 +0700

    edit file dosen dan mahasiswa di branch dev1

--- 04-Git Merge Conflict/dosen/dosen.py
+++ 04-Git Merge Conflict/dosen/dosen.py
@@ -1,3 +1,5 @@
-dosen = ["mata kuliah", "telephone"]
+dosen = ["nama", "mata kuliah", "telephone"]
 
-print(f"dosen mengajar {dosen[2]}, apabila ada pertanyaan hubungi {dosen[1]}")
+print(
+    f"dosen {dosen[0]}, mengajar {dosen[1]}, apabila ada pertanyaan hubungi {dosen[1]}"
+)
--- 04-Git Merge Conflict/mahasiswa/mahasiswa.py
+++ 04-Git Merge Conflict/mahasiswa/mahasiswa.py
@@ -1,3 +1,5 @@
-mahasiswa = ["nama", "NIM"]
+mahasiswa = ["nama", "NIM", "IPK"]
 
-print(f"mahasiswa baru {mahasiswa[0]}, memiliki NIM {mahasiswa[1]}")
+print(
+    f"mahasiswa baru {mahasiswa[0]}, memiliki NIM {mahasiswa[1]}, memiliki IPK {mahasiswa[2]}"
+)
